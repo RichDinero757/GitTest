d040a4db89b6fc1399d174ebfb8aca9e67c5ef22
Richard.Scott <Richard.Scott@libtax.com>
2014-04-03 14:43:40 -0400

    Testing Commit to Local Git

---
+++ GitTestApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitTestApplication.Startup))]
+namespace GitTestApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
